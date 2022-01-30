8d52bc401f32995a36353a5d2f78ee43d5475b4f
lcenriquez <luiscarlos_enriquez@me.com>
2022-01-30 10:52:24 -0600

    lang: change app language to English

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,23 @@
+require('dotenv').config();
+const axios = require('axios');
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 
 const router = Router();
+
+router.get('/', async (req, res) => {
+  let countries;
+  try {
+    let response = await axios.get(`${process.env.API_URL}/all`);
+    countries = await response.data;
+  } catch(err) {
+    console.log(err);
+  }
+
+  res.json(countries);
+});
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -5,8 +5,8 @@ function App() {
     <div className={style.landingPageContainer}>
       <div className={style.banner}>
         <h1>Henry Countries</h1>
-        <h2>Por Luis Carlos Enríquez</h2>
-        <button className={style.cta}>Comenzar</button>
+        <h2>Made by Luis Carlos Enríquez</h2>
+        <button className={style.cta}>Start</button>
       </div>
     </div>
   );
