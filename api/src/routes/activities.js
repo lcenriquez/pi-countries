c2f78746c89a34f60bb2e2732b330a251149fa92
lcenriquez <luiscarlos_enriquez@me.com>
2022-01-30 22:23:36 -0600

    feat (temp): populate middle table whupon Activity creation
    Links created Activity to a hardcoded Country

--- api/src/routes/activities.js
+++ api/src/routes/activities.js
@@ -1,15 +1,18 @@
 const express = require('express');
-const { Activity } = require('../db');
+const { Activity, Country } = require('../db');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  await Activity.create({
+  let activity = await Activity.create({
     name: 'Test',
     difficulty: 1,
     duration: '50m',
     season: 'Spring',
   });
+  let countries = [];
+  countries.push(await Country.findByPk('DEU'))
+  await activity.setCountries(countries);
   let activities = await Activity.findAll();
   res.json(activities);
 });
