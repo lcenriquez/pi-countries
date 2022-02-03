4e788fa9ae368c95530278a5c2a1077501511a97
lcenriquez <luiscarlos_enriquez@me.com>
2022-02-03 12:01:03 -0600

    build: setup api for deploy

--- api/src/db.js
+++ api/src/db.js
@@ -9,7 +9,11 @@ const {
 const sequelize = new Sequelize(process.env.DATABASE_URL || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  ssl: true
+  ssl: true,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: true
+  }
 });
 const basename = path.basename(__filename);
 
