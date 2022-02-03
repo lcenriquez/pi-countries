92ed1feaa714c3d5b39bb30cd8d8cf2ff194d0ad
lcenriquez <luiscarlos_enriquez@me.com>
2022-02-03 13:21:37 -0600

    style: add basic css to PaginationSelector component

--- client/src/components/PaginationSelector/PaginationSelector.jsx
+++ client/src/components/PaginationSelector/PaginationSelector.jsx
@@ -1,12 +1,14 @@
-import { useEffect, useState } from "react";
+import style from './PaginationSelector.module.css';
 
 export default function PaginationSelector({ array, currentPage, setCurrentPage, totalPages }) {
   return (
-    <div>
+    <div className={style.paginationContainer}>
       <p>Showing {array.length} items</p>
-      <p>Page {currentPage} of {totalPages}</p>
-      <button onClick={() => currentPage > 1 ? setCurrentPage(currentPage - 1) : null}>Prev</button>
-      <button onClick={() => currentPage < totalPages ? setCurrentPage(currentPage + 1) : null}>Next</button>
+      <div className={style.navControls}>
+        <button onClick={() => currentPage > 1 ? setCurrentPage(currentPage - 1) : null}>Prev</button>
+        <p>Page {currentPage} of {totalPages}</p>
+        <button onClick={() => currentPage < totalPages ? setCurrentPage(currentPage + 1) : null}>Next</button>
+      </div>
     </div>
   );
 }
