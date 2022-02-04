7acf86fe607649223dcafa6014246e5276825bc9
lcenriquez <luiscarlos_enriquez@me.com>
2022-02-03 20:13:34 -0600

    feat: getAllActivities method for fetching activities
    Fetches all activities from API

--- client/src/adapters/api/activities.js
+++ client/src/adapters/api/activities.js
@@ -1,3 +1,14 @@
+export async function getAllActivities() {
+  let activities;
+  try {
+    activities = await fetch(`${process.env.REACT_APP_API_URL}/activities`);
+    activities = activities.json();
+  } catch (error) {
+    console.log("GET activities Error:", error);
+  }
+  return activities;
+}
+
 export async function createActivity(activity) {
   let createdActivity = await fetch(`${process.env.REACT_APP_API_URL}/activities`, {
     method: 'post',
