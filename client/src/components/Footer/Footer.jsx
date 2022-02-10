808a6fbacfbd0d2ff8c8bd53ac449c011f85a47a
lcenriquez <luiscarlos_enriquez@me.com>
2022-02-09 21:19:59 -0600

    feat: add Footer component
    Created Footer component and added it to Layout

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import style from './Footer.module.css';
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>Created with ❤️ by: <a href="https://github.com/lcenriquez">Luis Carlos Enríquez</a></p>
+    </footer>
+  );
+}
--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,10 +1,12 @@
 import Nav from "../Nav/Nav";
+import Footer from "../Footer/Footer";
 
 export default function Layout({ children }) {
   return (
     <div>
       <Nav />
       {children}
+      <Footer />
     </div>
   );
 }
