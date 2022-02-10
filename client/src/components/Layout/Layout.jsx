78e68d64947148e31c38aa37a1ee820a598fd948
lcenriquez <luiscarlos_enriquez@me.com>
2022-02-10 15:45:37 -0600

    fix: always show footer at the bottom of the page
    Show footer at the bottom regardless content shown

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -2,6 +2,7 @@ import Nav from "../Nav/Nav";
 import Footer from "../Footer/Footer";
 
 export default function Layout({ children }) {
+  document.body.classList.add('appBody');
   return (
     <div>
       <Nav />
