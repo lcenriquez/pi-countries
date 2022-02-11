11856cf102d5f91e9811fc43c6a457484cf60dfd
lcenriquez <luiscarlos_enriquez@me.com>
2022-02-10 18:51:27 -0600

    fix: open footer link in a new tab

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import style from './Footer.module.css';
 export default function Footer() {
   return (
     <footer>
-      <p>Created with ❤️ by: <a href="https://github.com/lcenriquez">Luis Carlos Enríquez</a></p>
+      <p>Created with ❤️ by <a href="https://github.com/lcenriquez" target="_blank" rel="noreferrer">Luis Carlos Enríquez</a></p>
     </footer>
   );
 }
