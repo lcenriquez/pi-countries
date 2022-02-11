0cfcd608ad22a4614eee96760f6af7aea0e996c8
lcenriquez <luiscarlos_enriquez@me.com>
2022-02-11 09:38:20 -0600

    fix: use className in footer <p> tag

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import style from './Footer.module.css';
 export default function Footer() {
   return (
     <footer>
-      <p class={style.message}>Created with ❤️ by <a href="https://github.com/lcenriquez" target="_blank" rel="noreferrer">Luis Carlos Enríquez</a></p>
+      <p className={style.message}>Created with ❤️ by <a href="https://github.com/lcenriquez" target="_blank" rel="noreferrer">Luis Carlos Enríquez</a></p>
     </footer>
   );
 }
