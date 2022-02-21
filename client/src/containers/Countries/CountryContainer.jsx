513491ca0cbd5a0af4f410413b5381a974f91d54
lcenriquez <luiscarlos_enriquez@me.com>
2022-02-20 20:22:37 -0500

    feat: show not found when country does not exist
    Redirect to NotFound when CountryContainer does not find the requested country from id params

--- client/src/containers/Countries/CountryContainer.jsx
+++ client/src/containers/Countries/CountryContainer.jsx
@@ -1,16 +1,23 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { getCountry } from "../../adapters/api/countries";
 import Country from "../../components/Countries/Country";
 
 export default function CountryContainer() {
   const [ country, setCountry ] = useState({});
   const params = useParams();
+  const navigate = useNavigate();
   let id = params.id;
 
   useEffect(() => {
-    getCountry(id).then(country => setCountry(country));
-  },[]);
+    getCountry(id).then(country => {
+      if (country.hasOwnProperty('id')) {
+        setCountry(country);
+      } else {
+        navigate('/not-found');
+      }
+    });
+  },[id, navigate]);
 
   return (
     <div className="container">
